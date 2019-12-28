74d073fcd09181e28171ef884fb820b0ec21ddfb
Auz <oo7marcusaurelius@gmail.com>
2019-12-28 10:56:31 +0530

    sending data from client and server with real time interface

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,14 @@
-io()
+const socket=io();
+
+//recieving data on client side
+socket.on('message',(message)=>{
+    console.log(message);
+});
+
+//sending data from client side to server
+document.querySelector('#message-form').addEventListener('submit',(e)=>{
+    e.preventDefault() //preventing deafult browser refresh 
+
+    const message=e.target.elements.message.value ;
+    socket.emit('sendMessage',message);
+});
--- src/index.js
+++ src/index.js
@@ -8,11 +8,20 @@ const server=http.createServer(app);
 const io=socketio(server);
 
 const port=process.env.PORT || 3000;
+//served public files statically
 const publicDirectoryPath=path.join(__dirname,'../public');
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection',()=>{
+//on new connection
+io.on('connection',(socket)=>{
     console.log('New connection');
+  
+    //server sending message
+    socket.emit('messgae','Welcome');
+    //when receiving data
+    socket.on('sendMessage',(message)=>{
+        io.emit('message',message); //emitting message event with message data
+    });
 });
 server.listen(port,()=>{
     console.log(`Server listening on port ${port}`);
